f8907289aaa998cd52e6b72661b4a3f8062cdaa0
John Lång <john.lang@contravariant.fi>
2024-12-03 19:04:23 +0200

    Add test data

--- Day01.hs
+++ Day01.hs
@@ -10,6 +10,19 @@ import Data.Text (Text, lines, unpack, words)
 import Data.Text.IO (readFile)
 import Prelude hiding (lines, readFile, words)
 
+getInput :: IO Text
+getInput = readFile "input-01"
+
+example :: IO Text
+example =
+    return . pack $
+        "3   4\n\
+        \4   3\n\
+        \2   5\n\
+        \1   3\n\
+        \3   9\n\
+        \3   3"
+
 sortInputs :: Text -> ([Int], [Int])
 sortInputs input =
     let split = \case
