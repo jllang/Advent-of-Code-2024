30cdb90b01a8e5e90b2379497596e823e9092af3
John Lång <john.lang@contravariant.fi>
2024-12-12 12:46:07 +0200

    Solve day 7 task 2

--- Day07.hs
+++ Day07.hs
@@ -2,26 +2,26 @@
 
 module Day07 where
 
-import Control.Monad (forM_)
 import Data.Function ((&))
 import Data.Text (Text, pack, unpack)
 import qualified Data.Text as Text
 import Data.Text.IO (readFile)
-import Debug.Trace
 import Prelude hiding (readFile)
 
-data Expr = Const Int | Add Expr Expr | Mul Expr Expr
+data Expr = Const Int | Add Expr Expr | Mul Expr Expr | Concat Expr Expr
 
 instance Show Expr where
     show (Const n) = show n
     show (Add x y) = "(" ++ show x ++ " + " ++ show y ++ ")"
     show (Mul x y) = "(" ++ show x ++ " * " ++ show y ++ ")"
+    show (Concat x y) = "(" ++ show x ++ " || " ++ show y ++ ")"
 
 eval :: Expr -> Int
 eval e = case e of
     Const n -> n
     Add x y -> eval x + eval y
     Mul x y -> eval x * eval y
+    Concat x y -> read (show (eval x) ++ show (eval y))
 
 type Input = [(Int, [Int])]
 
@@ -52,11 +52,11 @@ parse t =
         os = map (map tread . tail . Text.split (== ' ') . Text.takeWhileEnd p) ls
      in zip rs os
 
-task1 :: Input -> Int
-task1 =
+solve :: [Expr -> Expr -> Expr] -> Input -> Int
+solve ops =
     let exprs = \case
             [x] -> [Const x]
-            (x : xs) -> [op y (Const x) | op <- [Add, Mul], y <- exprs xs]
+            (x : xs) -> [op y (Const x) | op <- ops, y <- exprs xs]
         valid (z, xs) =
             reverse xs
                 & exprs
@@ -64,9 +64,14 @@ task1 =
                 & elem z
      in sum . map fst . filter valid
 
+task1 :: Input -> Int
+task1 = solve [Add, Mul]
+
+task2 :: Input -> Int
+task2 = solve [Add, Mul, Concat]
+
 main :: IO ()
 main = do
-    lists <- parse <$> getInput
-    putStrLn $ "task 1 answer: " <> show (task1 lists)
-
--- putStrLn $ "task 2 answer: " <> show (task2 lists)
+    input <- parse <$> getInput
+    putStrLn $ "task 1 answer: " <> show (task1 input)
+    putStrLn $ "task 2 answer: " <> show (task2 input)
