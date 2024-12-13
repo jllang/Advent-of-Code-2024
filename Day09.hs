dfafaa9c5ed0e07f72421a9ffdb1e03d3c6efefc
John Lång <john.lang@contravariant.fi>
2024-12-13 13:30:59 +0200

    Split day 9 task 1 into two parts

--- Day09.hs
+++ Day09.hs
@@ -11,6 +11,7 @@ import qualified Data.Vector.Unboxed as Vector
 import Prelude hiding (readFile)
 
 data State = File | Free
+type Input = Vector (Int, Int)
 
 getInput :: IO Text
 getInput = readFile "input-09"
@@ -18,12 +19,23 @@ getInput = readFile "input-09"
 example :: IO Text
 example = return $ pack "2333133121414131402"
 
-parse :: Text -> Vector (Int, Int)
+parse :: Text -> Input
 parse t =
     let tokenizer c (s, i, is) = case (s, digitToInt c) of
             (File, n) -> (Free, i + 1, (i, n) : is)
             (Free, n) -> (File, i, (-1, n) : is)
      in Text.foldr' tokenizer (File, 0, []) t
             & (\(_, _, is) -> is)
-            & reverse
             & Vector.fromList
+            & Vector.reverse
+
+compact :: Input -> Input
+compact = undefined
+
+checksum :: Input -> Int
+checksum =
+    let f j (i, n) = (+ i * (j + ((n - 1) * n `div` 2)))
+     in Vector.ifoldr' f 0
+
+task1 :: Input -> Int
+task1 = checksum . compact
