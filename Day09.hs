389d9e58e0347a8ac4f309325f201c5a1d9e17ee
John Lång <john.lang@contravariant.fi>
2024-12-13 20:26:29 +0200

    Simplify data representation and use the right fold for parsing

--- Day09.hs
+++ Day09.hs
@@ -1,17 +1,20 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Day09 where
 
 import Data.Bifunctor (first)
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 import Data.Function ((&))
 import Data.Text (Text, pack, unpack)
 import qualified Data.Text as Text
 import Data.Text.IO (readFile)
 import Data.Vector.Unboxed (Vector)
 import qualified Data.Vector.Unboxed as Vector
+import Debug.Trace
 import Prelude hiding (readFile)
 
 data State = File | Free
-type Input = Vector (Int, Int)
+type Input = Vector Int
 
 getInput :: IO Text
 getInput = readFile "input-09"
@@ -21,21 +24,24 @@ example = return $ pack "2333133121414131402"
 
 parse :: Text -> Input
 parse t =
-    let tokenizer c (s, i, is) = case (s, digitToInt c) of
-            (File, n) -> (Free, i + 1, (i, n) : is)
-            (Free, n) -> (File, i, (-1, n) : is)
-     in Text.foldr' tokenizer (File, 0, []) t
+    let tokenizer (s, i, is) c =
+            case (s, digitToInt c) of
+                (File, n) -> (Free, i + 1, take n (repeat i) ++ is)
+                (Free, n) -> (File, i, take n (repeat (-1)) ++ is)
+        show' = \case
+            (-1) -> '.'
+            n -> intToDigit n
+     in Text.foldl' tokenizer (File, 0, []) t
             & (\(_, _, is) -> is)
             & Vector.fromList
             & Vector.reverse
+            & \v -> trace (map show' $ Vector.toList v) v
 
 compact :: Input -> Input
 compact = undefined
 
 checksum :: Input -> Int
-checksum =
-    let f j (i, n) = (+ i * (j + ((n - 1) * n `div` 2)))
-     in Vector.ifoldr' f 0
+checksum = Vector.ifoldr' (\j i -> (+ j * i)) 0
 
 task1 :: Input -> Int
 task1 = checksum . compact
